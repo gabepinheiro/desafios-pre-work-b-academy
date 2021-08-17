1db6b41adeb478ba9c55ce96d22a21f456bcce9f
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-17 12:35:54 -0300

    4th exercise done

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,15 @@
 import './style.css'
 
-document.querySelector('[data-js=app]').innerHTML = `
+const $App = document.querySelector('[data-js=app]')
+const $Link = document.querySelector('[data-js=link]')
+
+$App.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+$Link.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  $App.classList.toggle('isHidden')
+})
