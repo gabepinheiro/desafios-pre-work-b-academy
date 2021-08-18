a586fdc562d1e96a5d8edaa0bb8f522486c874c6
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-18 09:09:26 -0300

    Add imutable

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -5,10 +5,13 @@ const john = {
   hobbies: ['Surf', 'Design'],
 }
 
-const jane = john
-
-jane.name = 'Jane'
-jane.hobbies.push('MuayThai', 'Programming')
+const jane = {
+  ...john,
+  name: 'Jane',
+  hobbies: [...john.hobbies, 'MuayThai', 'Programming']
+}
 
 console.log('John:', john)
 console.log('Jane:', jane)
+
+console.log(john === jane)
