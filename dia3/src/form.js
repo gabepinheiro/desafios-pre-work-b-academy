98129173d430837f19964ece727f51847319235e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-19 13:29:04 -0300

    Day03: 2nd exercise done

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,3 +1,4 @@
+const $Form = document.querySelector('[data-js="form"]')
 const $InputName = document.querySelector('[data-js="name"]')
 
 const filterWords = word => {
@@ -17,3 +18,52 @@ const capitalizeName = (name) => {
 $InputName.addEventListener('input', e => {
   e.target.value = capitalizeName(e.target.value.toLowerCase().split(' '))
 })
+
+const renderSelect = () => {
+  const divSelect = document.createElement('div')
+
+  const select = document.createElement('select')
+  select.setAttribute('multiple', '')
+  select.setAttribute('data-js', 'colors-select')
+
+  select.addEventListener('change', renderColorsSelect)
+
+  const colors = ['blue', 'green', 'red', 'black', 'yellow']
+  colors.forEach((color) => {
+    const option = document.createElement('option')
+    option.setAttribute('value', color)
+    option.textContent = color
+
+    select.appendChild(option)
+  })
+
+  divSelect.appendChild(select)
+
+  $Form.appendChild(divSelect)
+}
+
+
+const renderColorsSelect = (e) => {
+  let container = document.querySelector('[data-js="container-colors"]')
+
+  if(!!container){
+    container.innerHTML = ''
+  } else {
+    container = document.createElement('div')
+    container.setAttribute('data-js', 'container-colors')
+    container.className = 'container-colors'
+  }
+
+  const $ColorsSelect = document.querySelector('[data-js="colors-select"]')
+
+  Array.from($ColorsSelect.selectedOptions).forEach(el => {
+    const boxColor = document.createElement('div')
+    boxColor.className = `box-color ${el.value}`
+
+    container.appendChild(boxColor)
+  })
+
+  $Form.appendChild(container)
+}
+
+renderSelect()
